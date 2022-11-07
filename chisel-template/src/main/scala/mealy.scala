81b43da0e42a55d78f6891074b9131157978f93e
ChenlinShi <scleverlin@Gmail.com>
2022-11-07 22:24:20 +0900

    duet 11.7

---
+++ chisel-template/src/main/scala/mealy.scala
@@ -0,0 +1,63 @@
+// Mealy machine has
+
+import chisel3._
+
+import chisel3.util._
+
+case class BinaryMealyParams(
+  // number of states
+  nStates: Int,
+  // initial state
+  s0: Int,
+  // function describing state transition
+  stateTransition: (Int, Boolean) => Int,
+  // function describing output
+  output: (Int, Boolean) => Int
+) {
+  require(nStates >= 0)
+  require(s0 < nStates && s0 >= 0)
+}
+  
+def stateTransition(state: Int, in: Boolean): Int = {
+  if (in) {
+    1
+  } else {
+    0
+  }
+}
+def output(state: Int, in: Boolean): Int = {
+  if (state == 2) {
+    return 0
+  }
+  if ((state == 1 && !in) || (state == 0 && in)) {
+    return 1
+  } else {
+    return 0
+  }
+
+}
+
+class BinaryMealy(val mp: BinaryMealyParams) extends Module {
+  val io = IO(new Bundle {
+    val in = Input(Bool())
+    val out = Output(UInt())
+  })
+
+  val state = RegInit(UInt(), mp.s0.U)
+
+  // output zero if no states
+  io.out := 0.U
+  for (i <- 0 until mp.nStates) {
+    when (state === i.U) {
+      when (io.in) {
+        state  := mp.stateTransition(i, true).U
+        io.out := mp.output(i, true).U
+      }.otherwise {
+        state  := mp.stateTransition(i, false).U
+        io.out := mp.output(i, false).U
+      }
+    }
+  }
+}
+
+// example from https://en.wikipedia.org/wiki/Mealy_machine
